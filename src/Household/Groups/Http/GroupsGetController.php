489fddce07f166138edf933eee3f37e3ec4c02f5
dominx99 <dominx992@gmail.com>
2022-08-04 22:05:32 +0200

    Display only groups that user belongs to

--- src/Household/Groups/Http/GroupsGetController.php
+++ src/Household/Groups/Http/GroupsGetController.php
@@ -8,18 +8,20 @@ use App\Household\Groups\Infrastructure\Persistence\GroupRepository;
 use App\Shared\Http\Symfony\ApiController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 final class GroupsGetController extends ApiController
 {
-    public function __construct(private GroupRepository $repository)
+    public function __construct(private GroupRepository $repository, private TokenStorageInterface $tokenStorage)
     {
     }
 
     #[Route('api/v1/groups', methods: ['GET'])]
     public function __invoke(): Response
     {
-        return $this->respond(
-            $this->repository->findAll(),
-        );
+        /** @var \App\Auth\Domain\User $user */
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        return $this->respond($user->getUserGroups());
     }
 }
